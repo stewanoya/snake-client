31b156bbb9a693f998e00070a77f950a27555bd3
stew anoya <stewartanoya@gmail.com>
2021-11-03 13:49:55 +0000

    moved connection function into a different file

--- play.js
+++ play.js
@@ -1,17 +1,3 @@
-const net = require("net");
+const connect = require("./client.js");
 
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-  conn.setEncoding("utf8");
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
-
-console.log("Connecting...");
 connect();
