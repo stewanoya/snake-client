270ab0faaa75da8f0a433f0720eda6083b12e7c8
stew anoya <stewartanoya@gmail.com>
2021-11-03 15:42:57 +0000

    seperated input functions into input file

--- play.js
+++ play.js
@@ -1,22 +1,6 @@
 const connect = require("./client.js");
+const setupInput = require("./input.js");
 
 connect();
-
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = () => {
-  process.stdin.on("data", (key) => {
-    if (key === "\u0003") {
-      process.exit();
-    }
-  });
-};
 
 setupInput();
