bc53addce0255934f739491f72658105a9fd84d3
stew anoya <stewartanoya@gmail.com>
2021-11-03 13:59:48 +0000

    added names to snakes

--- client.js
+++ client.js
@@ -9,6 +9,10 @@ const connect = function () {
   conn.on("data", (data) => {
     console.log(data);
   });
+  conn.on("connect", (msg) => {
+    console.log("Connected to game server!");
+    conn.write("Name: STU");
+  });
 
   return conn;
 };
